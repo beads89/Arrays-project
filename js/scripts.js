fe7b6bc39be6d69223dfaa727879885375405624
Brandon Eads <brandon181989@hotmail.com>
2021-06-23 22:36:08 -0700

    pass third test

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,8 @@ function numberTranslate(number) {
   for (let i = 0; i <= number; i++) {
     if (i.toString().includes(3)) {
       numberDisplay.push("Won't you be my neighbor?");
-    } else if {
-      (i.toString().includes(2)) {
+    } else if (i.toString().includes(2)) {
         numberDisplay.push("Boop!");
-      }
-    }
     } else {
       numberDisplay.push(i);
     } 
